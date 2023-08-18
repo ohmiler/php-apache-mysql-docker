473a4d44053cd46ab0b1c8346b53060535950a28
Patiphan Phengpao <misterpatipan123@gmail.com>
2023-08-18 15:33:27 +0700

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,12 +1,12 @@
 drop table if exists `users`;
-create table USERS (
+create table users (
 	id int,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
 	age int
 );
-insert into USERS (id, first_name, last_name, age) values (1, 'john', 'doe', 25);
-insert into USERS (id, first_name, last_name, age) values (2, 'jane', 'doe', 30);
-insert into USERS (id, first_name, last_name, age) values (3, 'jim', 'doe', 35);
-insert into USERS (id, first_name, last_name, age) values (4, 'jay', 'doe', 40);
-insert into USERS (id, first_name, last_name, age) values (5, 'joe', 'doe', 45);
+insert into users (id, first_name, last_name, age) values (1, 'john', 'doe', 25);
+insert into users (id, first_name, last_name, age) values (2, 'jane', 'doe', 30);
+insert into users (id, first_name, last_name, age) values (3, 'jim', 'doe', 35);
+insert into users (id, first_name, last_name, age) values (4, 'jay', 'doe', 40);
+insert into users (id, first_name, last_name, age) values (5, 'joe', 'doe', 45);
